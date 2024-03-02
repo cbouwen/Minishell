5bb5cec07f5567547906d70983de96e5835f1739
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-02 21:08:38 +0100

    Added the first part of the echo function. We should add a FLAG option to the enum

--- src/executor/echo.c
+++ src/executor/echo.c
@@ -2,4 +2,27 @@
 
 #include "../../inc/minishell.h"
 
-void echo()
+void	echo(t_token *tokens)
+{
+	t_token	*temp;
+	int		n_flag;
+
+	temp = tokens;
+	n_flag = 0;
+	(void)n_flag;
+	/*if (temp->type == FLAG)
+	{
+		if (ft_strcmp(temp->str, "-n") == 0)
+			n_flag = 1;
+		temp = temp->next;
+	}*/
+	while (temp)
+	{
+		if (temp->type == ARG)
+			ft_putstr_fd(temp->str, temp->output);
+		temp = temp->next;
+	}
+	ft_putchar_fd('\n', temp->output);
+	/*if (n_flag == 0)
+			ft_putchar_fd('\n', temp->output);*/
+}
