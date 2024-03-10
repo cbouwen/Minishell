5e63fdd918366f3d593bd7847b3506874bc83d18
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-10 16:41:44 +0100

    added new functions to extract env var name and env var value

--- inc/executor.h
+++ inc/executor.h
@@ -12,6 +12,8 @@ int		executor(t_token *tokens, t_environment *env);
 void	print_env(t_token *tokens, t_environment *env);
 void	env_error_msg(t_token *tokens);
 int		export_var(t_token *tokens, t_environment *env);
-int		check_env_val_exists(t_token *tokens, t_environment *env, char **var_name);
+int		check_env_val_exists(t_environment *env, char *var_name);
+int		extract_name(t_token *tokens, char **var_name);
+int		extract_value(t_token *tokens, char **var_name);
 
 #endif
--- src/executor/export.c
+++ src/executor/export.c
@@ -21,13 +21,15 @@ int	export_var(t_token *tokens, t_environment *env)
 	temp = tokens;
 	temp_env = env;
 	var_name = NULL;
-	if (check_env_val_exists(temp, temp_env, &var_name) == 1)
-		printf("%s\n", var_name);
+	extract_name(temp, &var_name);
+	extract_value(temp, &var_name);
+	if (check_env_val_exists(temp_env, var_name) == 1)
+		printf("var_name: %s\n", var_name);	
 	free(var_name);
 	return (0);	
 }
 
-int	check_env_val_exists(t_token *tokens, t_environment *env, char **var_name)
+int	extract_name(t_token *tokens, char **var_name)
 {
 	int		sign_loc;
 	char	*value;
@@ -38,12 +40,37 @@ int	check_env_val_exists(t_token *tokens, t_environment *env, char **var_name)
 	{
 		value = ft_strchr(tokens->str, '=');
 		if (!value)
-			return (2);
+			return (1);
 	}
 	else
-		return (2);
+		return (1);
 	sign_loc = value - tokens->str;
 	*var_name = ft_substr(tokens->str, 0, sign_loc);
+	return (0);
+}
+
+int	extract_value(t_token *tokens, char **var_name)
+{
+	int		sign_loc;
+	char	*value;
+
+	sign_loc = 0;
+	value = NULL;
+	if (tokens->type == ARG)
+	{
+		value = ft_strchr(tokens->str, '=');
+		if (!value)
+			return (1);
+	}
+	else
+		return (1);
+	sign_loc = value - tokens->str;
+	*var_name = ft_substr(tokens->str, sign_loc + 1, ft_strlen(tokens->str) - sign_loc);
+	return (0);
+}
+
+int	check_env_val_exists(t_environment *env, char *var_name)
+{
 	while (env)
 	{
 		if (ft_strcmp(*var_name, env->name) == 0)
