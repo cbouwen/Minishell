4ec7ea680e10e738580ca960a62a649bccfce1dc
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-17 21:13:06 +0100

    Fixed small error in ft_error

--- src/utils/error_utils.c
+++ src/utils/error_utils.c
@@ -7,7 +7,7 @@ extern int	g_err;
 int	ft_error(char *str, int err_no)
 {
 	g_err = errno;
-	if (errno >= 133)
+	if (errno <= 133)
 		printf("minishell: %s\n", strerror(err_no));
 	if (str)
 		printf("%s", str);
