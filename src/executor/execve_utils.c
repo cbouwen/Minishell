9b20c6172555d63f6c9de06310d1792cd5ceccf9
Matisse Legendre <legendre.matisse@gmail.com>
2024-03-19 18:59:03 +0100

    First version of EXECVE is done. All tokens now get pushed into a char array up to the redirect or pipe. Plan is to now create a series of functions that determine wether there are pipes or redirects and to handle those accordingly

--- src/executor/execve_utils.c
+++ src/executor/execve_utils.c
@@ -6,26 +6,100 @@
 /*   By: mlegendr <mlegendr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 19:08:54 by mlegendr          #+#    #+#             */
-/*   Updated: 2024/03/18 19:09:39 by mlegendr         ###   ########.fr       */
+/*   Updated: 2024/03/19 18:54:41 by mlegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-int	execve_utils(char *path, char **args, char **env)
+size_t	count_tokens(t_token *tokens)
 {
-	pid_t	pid;
+	t_token	*temp;
+	int		i;
+
+	temp = tokens;
+	i = 0;
+	while (temp && temp->type != PIPE && temp->type != REDIRECT)
+	{
+		i++;
+		temp = temp->next;
+	}
+	return (i);
+}
+
+int	execve_prep(t_token *tokens)
+{
+	t_args	*args;
 	int		status;
 
+	args = malloc(sizeof(t_args));
+	status = fill_args(args, tokens);
+	if (status != 0)
+	{
+		free_args(args);
+		return (ft_error(NULL, status));
+	}
+	status = run_execve(args);
+	free_args(args);
+	return (ft_error(NULL, status));
+}
+
+int	fill_args(t_args *args, t_token *tokens)
+{
+	t_token	*temp;
+	size_t		i;
+	size_t		count;
+
+	temp = tokens;
+	args->arg_array = malloc(sizeof(char *) * (count_tokens(temp) + 1));
+	if (!args->arg_array)
+		return (ft_error(NULL, 12));
+	i = 0;
+	temp = tokens;
+	count = count_tokens(temp);
+	while (temp && i < count)
+	{
+		args->arg_array[i] = ft_strdup(temp->str);
+		if (!args->arg_array[i])
+			return (ft_error(NULL, 12));
+		i++;
+		temp = temp->next;
+	}
+	args->arg_array[i] = NULL;
+	return (0);
+}
+
+int run_execve(t_args *args)
+{
+	int	status;
+	pid_t	pid;
+	
+	status = 0;
 	pid = fork();
 	if (pid == 0)
 	{
-		if (execve(path, args, env) == -1)
+		if (execve(args->arg_array[0], args->arg_array, NULL) == -1)
 			exit(1);
 	}
 	else
 		waitpid(pid, &status, 0);
-	if (WIFEXITED(status))
-		return (WEXITSTATUS(status));
-	return (1);
+	return (status);
+}
+
+int	free_args(t_args *args)
+{
+	int	i;
+
+	i = 0;
+	if (args)
+	{
+		while (args->arg_array[i])
+		{
+			free(args->arg_array[i]);
+			i++;
+		}
+		free(args->arg_array);
+		free(args);
+	}
+	return (0);
 }
