aaba73d89380bf0efce9f3548eb1367c71752ede
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-24 00:02:02 +0100

    Update redirect_utils.c

--- src/executor/redirect_utils.c
+++ src/executor/redirect_utils.c
@@ -63,7 +63,7 @@ void	check_file_exists(t_args *args)
 	char *path = getenv("PWD");
 	char *file_path = ft_strjoin(path, "/");
 	file_path = ft_strjoin_free(file_path, args->file, true);
-	args->file = file_path;
+	args->file = ft_strdup(file_path);
 	int fd = open(args->file, O_RDWR | O_CREAT | O_TRUNC, 0644);
 	args->fd = fd;
 	if (fd < 0)
