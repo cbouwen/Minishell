5236160be62f867702f6a69efe6354742086795a
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-26 00:23:21 +0100

    Update heredoc_setup.c

--- src/executor/redirections/heredoc_setup.c
+++ src/executor/redirections/heredoc_setup.c
@@ -60,7 +60,7 @@ int	heredoc_no_redirect(t_token *tokens, t_args *args)
 	args->fd = open(path, O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (args->fd == -1)
 		return (ft_error("heredoc_no_redirect: open error\n", 3));
-	while (line = readline("heredoc> "))
+	while (line = readline("heredoc> ") != NULL)
 	{
 		if (ft_strcmp(line, args->delimiter) == 0)
 			free(line);
