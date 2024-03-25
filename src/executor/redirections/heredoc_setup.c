47f86c064e0fafca62a451c2d005b64cf0743c4e
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-26 00:20:47 +0100

    heredoc test

--- src/executor/redirections/heredoc_setup.c
+++ src/executor/redirections/heredoc_setup.c
@@ -22,9 +22,9 @@ int	setup_heredoc(t_token *tokens, t_args *args)
 	}
 	temp = temp->next;
 	if (!temp->next || temp->next->type != REDIRECT)
-		return (heredoc_no_redirect(temp, temp_args));
+		return (ft_error(NULL, heredoc_no_redirect(temp, temp_args)));
 	else
-		return (heredoc_redirect(temp, temp_args));
+		return (ft_error(NULL, heredoc_redirect(temp, temp_args)));
 }
 
 /*jump to next token*/
@@ -45,14 +45,26 @@ int	heredoc_no_redirect(t_token *tokens, t_args *args)
 {
 	t_token	*temp;
 	t_args	*temp_args;
+	char	*path;
 
 	temp = tokens;
 	temp_args = args;
-
-	(void)temp;
-	(void)temp_args;
-	printf("heredoc_no_redirect\n");
-	printf("delimiter: %s\n", args->delimiter);
+	path = ft_strdup("/tmp/heredoc_dump");
+	if (!path)
+		return (ft_error("heredoc_no_redirect: malloc error\n", 12));
+	args->fd = open(path, O_RDWR | O_CREAT | O_TRUNC, 0644);
+	if (args->fd == -1)
+		return (ft_error("heredoc_no_redirect: open error\n", 3));
+	while (line = readline("heredoc> "))
+	{
+		if (ft_strcmp(line, args->delimiter) == 0)
+			free(line);
+		write(args->fd, line, ft_strlen(line));
+		write(args->fd, "\n", 1);
+		free(line);
+	}
+	close(args->fd);
+	free(path);
 	return (0);
 }
 
