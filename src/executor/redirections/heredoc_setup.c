daec15d27169b6defdbcdf16af8f000f90ade622
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-25 20:26:30 +0100

    small heredoc setup test

---
+++ src/executor/redirections/heredoc_setup.c
@@ -0,0 +1,79 @@
+/*header pls*/
+
+#include "../../../inc/minishell.h"
+
+int	setup_heredoc(t_token *tokens, t_args *args)
+{
+	t_token	*temp;
+	t_args	*temp_args;
+
+	temp = tokens;
+	temp_args = args;
+
+	/*jump to redirect*/
+	while (temp && temp->type != REDIRECT)
+		temp = temp->next;
+
+	/*check if there is a delimiter*/
+	if (!temp->next || temp->next->type != ARG)
+		return (ft_error("heredoc: syntax error 1\n", 3));
+	else
+	{
+		while (temp->next && temp->next->type == ARG)
+		{
+			if (args->delimiter)
+				args->delimiter = strjoin_free(args->delimiter, temp->next->str, 1);
+			else
+				args->delimiter = ft_strdup(temp->next->str);
+			temp = temp->next;
+		}
+	}
+
+	/*check for new redirect*/
+	if (!temp->next || temp->next->type != REDIRECT)
+		heredoc_no_redirect(temp, temp_args);
+	else
+		heredoc_redirect(temp, temp_args);
+	return (0);
+	/*jump to next token*/
+	/*temp = temp->next->next;
+	if (temp->type != REDIRECT || ft_strcmp(temp->str, "<<") == 0)
+		return (ft_error("heredoc: syntax error 2\n", 3));
+	else
+		determine_redirect(temp, temp_args);
+	temp = temp->next;
+	if (temp->type != ARG)
+		return (ft_error("heredoc: syntax error 3\n", 3));
+	else
+		args->file = ft_strdup(temp->str);
+	check_file_exists(temp_args);
+	return (0);*/
+}
+
+int	heredoc_no_redirect(t_token *tokens, t_args *args)
+{
+	t_token	*temp;
+	t_args	*temp_args;
+
+	temp = tokens;
+	temp_args = args;
+
+	(void)temp;
+	(void)temp_args;
+	printf("heredoc_no_redirect\n");
+	return (0);
+}
+
+int	heredoc_redirect(t_token *tokens, t_args *args)
+{
+	t_token	*temp;
+	t_args	*temp_args;
+
+	temp = tokens;
+	temp_args = args;
+
+	(void)temp;
+	(void)temp_args;
+	printf("heredoc_redirect\n");
+	return (0);
+}
--- src/executor/redirections/redirect_utils.c
+++ src/executor/redirections/redirect_utils.c
@@ -102,37 +102,6 @@ int	open_file(t_args *args)
 	return (ft_error(NULL, 0));
 }
 
-int	setup_heredoc(t_token *tokens, t_args *args)
-{
-	(void)tokens;
-	(void)args;
-	printf("heredoc\n");
-	return (0);
-	/*t_token	*temp;
-	t_args	*temp_args;
-
-	temp = tokens;
-	temp_args = args;
-	while (temp && temp->type != REDIRECT)
-		temp = temp->next;
-	if (!temp->next || temp->next->type != ARG)
-		return (ft_error("heredoc: syntax error 1\n", 3));
-	else
-		args->delimiter = ft_strdup(temp->next->str);
-	temp = temp->next->next;
-	if (temp->type != REDIRECT || ft_strcmp(temp->str, "<<") == 0)
-		return (ft_error("heredoc: syntax error 2\n", 3));
-	else
-		determine_redirect(temp, temp_args);
-	temp = temp->next;
-	if (temp->type != ARG)
-		return (ft_error("heredoc: syntax error 3\n", 3));
-	else
-		args->file = ft_strdup(temp->str);
-	check_file_exists(temp_args);
-	return (0);*/
-}
-
 /*int	heredoc_setup(t_token *token, t_args *args)
 {
 	int		fd;
