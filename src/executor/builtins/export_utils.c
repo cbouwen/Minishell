43442cf088878f95a922c8d4c2793e73db7e9a1a
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-26 13:03:19 +0100

    Update export_utils.c

--- src/executor/builtins/export_utils.c
+++ src/executor/builtins/export_utils.c
@@ -55,7 +55,7 @@ int	special_export(char *token_str, char **var_name, char **var_value)
 	(void)var_name;
 	(void)var_value;
 	printf("special export\n");
-	return (0);
+	return (1);
 	/*int		sign_loc;
 	char	*value;
 
