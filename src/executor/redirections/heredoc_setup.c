2e6a5a4ce9f1b2214c145175929e4a444ec12a7a
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-26 17:38:24 +0100

    Update heredoc_setup.c

--- src/executor/redirections/heredoc_setup.c
+++ src/executor/redirections/heredoc_setup.c
@@ -53,8 +53,8 @@ int	heredoc_no_redirect(t_token *tokens, t_args *args)
 	path = ft_strdup("/tmp/heredoc_dump");
 	if (!path)
 		return (ft_error("heredoc_no_redirect: strdup error\n", 3));
-	if (update_args(args, path) != 0)
-		return (ft_error("heredoc_no_redirect: update_args error\n", 3));
+	/*if (update_args(args, path) != 0)
+		return (ft_error("heredoc_no_redirect: update_args error\n", 3));*/
     args->fd = open("/tmp/heredoc_dump", O_RDWR | O_CREAT | O_TRUNC, 0644);
     if (args->fd == -1)
         return (ft_error("heredoc_no_redirect: open error\n", 3));
