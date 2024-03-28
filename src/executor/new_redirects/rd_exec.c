a81b85b640fe9e94cd2652f5b4f6a90c26c36f0f
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-28 01:55:21 +0100

    Update rd_exec.c

--- src/executor/new_redirects/rd_exec.c
+++ src/executor/new_redirects/rd_exec.c
@@ -43,7 +43,7 @@ int	input_redirection(t_token *tok, t_environment *env, t_args *arg, t_rd_collec
 	temp_rd->stdin = dup(STDIN_FILENO);
 	if (temp_rd->stdin == -1)
 		return (rd_error_handler(3, NULL, rd));
-	if (dup2(temp_rd->i_fd, temp_rd->stdin) == -1)
+	if (dup2(temp_rd->i_fd, STDIN_FILENO) == -1)
 		return (rd_error_handler(4, NULL, temp_rd));
 	close(temp_rd->i_fd);
 	status = prep_cmd(temp, temp_env, temp_args);
