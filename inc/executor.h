dcb3c477ebd7ed1b8d06ed59c6308e6c3f9caf68
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-28 02:14:48 +0100

    Update executor.h

--- inc/executor.h
+++ inc/executor.h
@@ -121,7 +121,7 @@ int			open_heredoc(char *input);
 int			free_rd(t_rd_collection *rd);
 int			rd_exec_setup(t_token *tok, t_environment *env, t_args *arg, t_rd_collection *rd);
 int			input_rd(t_token *tok, t_environment *env, t_args *arg, t_rd_collection *rd);
-int			output_rd(t_token *tok, t_environment *env, t_args *arg, t_rd_collection *rd)
+int			output_rd(t_token *tok, t_environment *env, t_args *arg, t_rd_collection *rd);
 
 void printf_args_env(t_args *args);
 int update_args(t_args *args, char *path);
