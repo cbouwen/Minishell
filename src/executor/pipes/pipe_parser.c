7f400c3d631aa84b69fe1bf1df2727327c0dbd68
Matisse Legendre <legendre.matisse@gmail.com>
2024-03-29 17:07:30 +0100

    Progress Fri 29 Mar 2024 05:07:29 PM CET

--- src/executor/pipes/pipe_parser.c
+++ src/executor/pipes/pipe_parser.c
@@ -52,7 +52,7 @@ int	run_piped_cmd(t_token **tokens, t_env *env, t_args *args)
 	t_args	*temp_args;
 	int		status;
 	int		pipe_count;
-	int		fd[2];
+	//int		fd[2];
 	int		in_fd = 0;
 
 	//temp = **tokens;
@@ -62,7 +62,7 @@ int	run_piped_cmd(t_token **tokens, t_env *env, t_args *args)
 	pipe_count = count_pipes(*tokens);
 	while (pipe_count >= 0)
 	{
-		pipe(fd);
+		pipe(temp_args->fd);
 		pid_t pid = fork();
 		if (pid == 0)
 		{
@@ -73,9 +73,9 @@ int	run_piped_cmd(t_token **tokens, t_env *env, t_args *args)
 			}
 			if (pipe_count > 0)
 			{
-				dup2(fd[1], 1);
+				dup2(temp_args->fd[1], 1);
 			}
-			close(fd[0]);
+			close(temp_args->fd[0]);
 			status = run_piped_execve(*tokens, temp_env, temp_args);
 			exit(status);
 		}
@@ -87,12 +87,12 @@ int	run_piped_cmd(t_token **tokens, t_env *env, t_args *args)
 		else
 		{
 			wait(NULL);
-			close(fd[1]);
+			close(temp_args->fd[1]);
 			if (in_fd != 0)
 			{
 				close(in_fd);
 			}
-			in_fd = fd[0];
+			in_fd = temp_args->fd[0];
 		}
 
 		move_to_next(tokens);
@@ -112,46 +112,6 @@ int run_piped_execve(t_token *tokens, t_env *env, t_args *args)
 	temp_env = env;
 	temp_args = args;
 	status = 0;
-	/*status = fill_args(temp_args, temp);
-	status = assemble_path(temp_args);
-	status = path_error_handler(status);
-	if (status == 2)
-		status = run_execve(temp_args);*/
 	status = run_basic_cmd(temp, temp_env, temp_args);
 	return (status);
 }
-
-/*int	run_piped_builtin(t_token *tokens, t_env *env, t_args *args)
-{
-	t_token	*temp;
-	t_env	*temp_env;
-	t_args	*temp_args;
-	int		status;
-
-	temp = tokens;
-	temp_env = env;
-	temp_args = args;
-	status = 0;
-	temp_args->fd[1] = open("/tmp/pipe", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (temp_args->fd[1] == -1)
-		return (3);
-	if (dup2(temp_args->fd[1], temp_args->fd[0]) == -1)
-		return (4);
-	run_builtin(temp, temp_env);
-	if (dup2(temp_args->fd[0], temp_args->fd[1]) == -1)
-		return (4);
-	close(temp_args->fd[1]);
-	return (status);
-
-
-
-
-
-
-
-	if (check_redirects(temp) == 1)
-		status = prep_cmd(temp, temp_env, NULL);
-	else
-		status = run_redirect(temp, temp_env, NULL);
-	return (status);
-}*/
