88d0a85e98a4cb6537739f521af805d7b5ecb8f9
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-31 00:49:55 +0100

    s

--- src/executor/pipes/pipe_parser.c
+++ src/executor/pipes/pipe_parser.c
@@ -78,6 +78,7 @@ int	run_piped_cmd(t_token **tokens, t_env *env, t_args *args)
 			args->in_fd = args->fd[0];
 		}
 		args->call_count++;
+		printf("call_count: %d\n", args->call_count);
 		move_to_next(tokens);
 		args->pipe_count--;
 	}
--- src/executor/redirects/rd_setup.c
+++ src/executor/redirects/rd_setup.c
@@ -113,7 +113,6 @@ int run_redirect(t_token *tokens, t_env *env, t_args *args)
 	temp_args = args;
 	status = 0;
 	debug_tm(tokens);
-	printf("call_count: %d\n", args->call_count);
 	if (redirect_syntax_check(temp) != 0)
 		return (1);
 	if (init_rd(temp, &rd) != 0)
