1ccec4de93495f5a117384943de9e32921968935
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-31 21:55:16 +0200

    Update pipe_parser.c

--- src/executor/pipes/pipe_parser.c
+++ src/executor/pipes/pipe_parser.c
@@ -93,6 +93,5 @@ int	run_cmd(t_token *tokens, t_env *env, t_args *args)
 	temp_args = args;
 	status = 0;
 	status = run_basic_cmd(temp, temp_env, temp_args);
-	free_args(temp_args);
 	return (status);
 }
