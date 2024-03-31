416e6fe5663cc2e2fe28ae99092083db4281d2f0
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-31 03:02:12 +0200

    Update pipe_parser.c

--- src/executor/pipes/pipe_parser.c
+++ src/executor/pipes/pipe_parser.c
@@ -64,6 +64,7 @@ int	run_piped_cmd(t_token **tokens, t_env *env, t_args *args)
 	while (args->pipe_count >= 0)
 	{
 		printf("call_count: %d\n", args->call_count);
+		debug_tm(*tokens);
 		pipe(args->fd);
 		pid = fork();
 		if (pid == 0)
@@ -79,7 +80,6 @@ int	run_piped_cmd(t_token **tokens, t_env *env, t_args *args)
 			args->in_fd = args->fd[0];
 		}
 		args->call_count++;
-		debug_tm(*tokens);
 		move_to_next(tokens);
 		args->pipe_count--;
 	}
