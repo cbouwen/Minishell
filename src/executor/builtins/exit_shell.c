59522e8acfa3f22a70f2930a6643bc52313b99f1
LegendreMatisse <legendre.matisse@gmail.com>
2024-04-01 00:31:22 +0200

    Update exit_shell.c

--- src/executor/builtins/exit_shell.c
+++ src/executor/builtins/exit_shell.c
@@ -4,10 +4,5 @@ int	exit_shell(t_token *tokens, t_env *env, t_args *args)
 {
 	if (args)
 		free_args(args);
-	if (tokens)
-		free_tokens(tokens);
-	if (env)
-		free_env(env);
-	rl_clear_history();
-	exit(0);
+	clean_exit(tokens, env, NULL);
 }
