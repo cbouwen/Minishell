2ef00e3bec54cb90c6d3e43cc7128b0bccc87d57
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-31 23:20:01 +0200

    Update cool_cat.c

--- src/executor/pipes/cool_cat.c
+++ src/executor/pipes/cool_cat.c
@@ -8,8 +8,10 @@ int	cool_cat(t_token **tokens, t_env *env, t_args *args)
 	t_env	*temp_env;
 	t_args	*temp_args;
 	int		status;
+	int		count;
 
 	status = 0;
+	count = 0;
 	temp = *tokens;
 	temp_env = env;
 	temp_args = args;
@@ -18,8 +20,12 @@ int	cool_cat(t_token **tokens, t_env *env, t_args *args)
 	while (temp)
 	{
 		if (check_cat_no_args(temp))
-			printf("cat no args\n");
+		{
+			count++;
+			move_to_next(tokens);
+		}
 		temp = temp->next;
 	}
+	printf("count: %d\n", count);
 	return (status);
 }
