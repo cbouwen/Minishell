c49899f075303a5553159d1c4fab014dd27d2c30
LegendreMatisse <legendre.matisse@gmail.com>
2024-03-31 18:26:11 +0200

    Update args_converter.c

--- src/executor/args_converter.c
+++ src/executor/args_converter.c
@@ -56,7 +56,7 @@ int	fill_env(t_args *args, t_env *env)
 		tmp = ft_strjoin(temp->name, "=");
 		if (!tmp)
 			return (ft_error("fill_env: malloc error\n", 12));
-		args->env_array[i] = strjoin_free(tmp, ft_strdup(temp->value), 1);
+		args->env_array[i] = strjoin_free(tmp, temp->value);
 		if (!args->env_array[i])
 			return (ft_error("fill_env: malloc error\n", 12));
 		i++;
